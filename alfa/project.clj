f8422c840cc8073542e202387ebda472b82b31b2
Sabda PS <sabdaps@gmail.com>
2022-01-18 01:31:29 +0700

    Update project.clj

--- alfa/project.clj
+++ alfa/project.clj
@@ -5,4 +5,5 @@
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [beoliver/clj-arangodb "0.0.9"]]
+  :source-paths ["src/clj" "dev"]
   :repl-options {:init-ns user})
