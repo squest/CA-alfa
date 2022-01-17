8e849f531750d2e62f0911c93cb21b6d91a1c756
Sabda PS <sabdaps@gmail.com>
2022-01-18 01:20:17 +0700

    trying arangodb

--- alfa/dev/user.clj
+++ alfa/dev/user.clj
@@ -1,1 +1,35 @@
-(ns user)
+(ns user
+  (:require [clj-arangodb.arangodb.core :as a]
+            [clj-arangodb.arangodb.databases :as d]
+            [clj-arangodb.arangodb.collections :as c]
+            [clj-arangodb.arangodb.adapter :as adapter])
+  (:import [com.arangodb Protocol]
+           [com.arangodb.entity BaseDocument]
+           [com.arangodb.velocypack VPackSlice]))
+
+(defonce conn (atom {}))
+
+(def default-config
+  {:useProtocol Protocol/VST
+   :user        "zenius"
+   :password    "mizone"
+   :host        ["127.0.0.1" 8529]})
+
+(def do-config
+  {:useProtocol Protocol/VST
+   :user        "root"
+   :host        ["128.199.252.210" 8528]})
+
+(defn connect!
+  ([config]
+   (reset! conn (a/connect config)))
+  ([] (connect! default-config)))
+
+(defn disconnect! []
+  (swap! conn a/shutdown))
+
+
+
+
+
+
